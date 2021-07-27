c8f4a7219e3253692da75d8d5f40ed00656600e5
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2021-07-27 16:34:25 +0100

    Add FourCircularPillarsLayout

--- Assets/Generation/GeomRep/Layouts/CircularPillarGeomLayout.cs
+++ Assets/Generation/GeomRep/Layouts/CircularPillarGeomLayout.cs
@@ -42,4 +42,49 @@ namespace Assets.Generation.GeomRep.Layouts
             return ret;
         }
     }
+
+    class FourCircularPillarsLayoutFactory : IGeomLayoutFactory
+    {
+        public IGeomLayout Create(INode n)
+        {
+            return new FourCircularPillarsGeomLayout(n.Position, n.Radius);
+        }
+
+        public IGeomLayout Create(DirectedEdge de)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FourCircularPillarsGeomLayout : GeomLayout
+    {
+        private readonly Vector2 m_position;
+        private readonly float m_rad;
+
+        public FourCircularPillarsGeomLayout(Vector2 position, float radius)
+        {
+            m_position = position;
+            m_rad = radius;
+        }
+
+        public override Loop MakeBaseGeometry()
+        {
+            return new Loop(new CircleCurve(m_position, m_rad));
+        }
+
+        public override LoopSet MakeDetailGeometry()
+        {
+            LoopSet ret = new LoopSet();
+
+            for (int i = 0; i < 4; i++)
+            {
+                float ang = Mathf.PI * 2 * i / 4;
+                Vector2 pos = m_position + new Vector2(Mathf.Sin(ang) * m_rad / 2, Mathf.Cos(ang) * m_rad / 2);
+
+                ret.Add(new Loop(new CircleCurve(pos, m_rad / 6, CircleCurve.RotationDirection.Reverse)));
+            }
+
+            return ret;
+        }
+    }
 }
