2d967fc69dab68c6d6b91b98908c4001019d40a9
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2021-08-07 17:52:01 +0100

    New files I am fed up with stashing and unstashing and which should not break anything

---
+++ Assets/Generation/GeomRep/LoopDisplayException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assets.Generation.GeomRep
+{
+    public class LoopDisplayException : Exception
+    {
+        public readonly Loop Loop1;
+        public readonly Loop Loop2;
+
+        public LoopDisplayException(Loop l1, Loop l2)
+        {
+            Loop1 = l1;
+            Loop2 = l2;
+        }
+    }
+}
